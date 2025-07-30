0fc92b1601a28d4ea21052150b0af851259ab482
MattHughson <matthew.hughson@lesappelsillimites.com>
2025-07-30 09:39:17 -0400

    change the url in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
 			<main style={{ width: '100%', height: '100%', margin: 0, padding: 0 }}>
 				{completed ? (
 					<iframe
-						src={process.env.APPURL}
+						src={`${process.env.APPURL}`}
 						style={{ width: '100%', height: '100%', border: 'none' }}
 						allowFullScreen
 					/>
