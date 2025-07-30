319f53d0ea742c881e3886411e585010428b9640
MattHughson <matthew.hughson@lesappelsillimites.com>
2025-07-30 09:50:39 -0400

    change the variable name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { useAppBridge } from '@/hooks';
 
 export default function Home() {
 	const { completed } = useAppBridge({ type: 'space-plugin', oauth: true });
+	console.log('app', process.env.APPURL);
 
 	return (
 		<>
@@ -14,7 +15,7 @@ export default function Home() {
 			<main style={{ width: '100%', height: '100%', margin: 0, padding: 0 }}>
 				{completed ? (
 					<iframe
-						src={`${process.env.APPURL}`}
+						src={`${process.env.NEXT_PUBLIC_APPURL}`}
 						style={{ width: '100%', height: '100%', border: 'none' }}
 						allowFullScreen
 					/>
